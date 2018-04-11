6b042c06e9ffa5d4210a3cd120939e8d17e27d25
unaoya <umezakinaoya@gmail.com>
2018-04-12 08:06:37 +0900

    finite union

--- finset.v
+++ finset.v
@@ -159,7 +159,7 @@ Proof.
   apply Union_intror.
   apply H.
 Qed.  
-Lemma finite_disjoint_union : forall (n : nat) (X Y : Ensemble U), cardinal X n -> Finite Y -> Disjoint U X Y -> Finite (Union U X Y).
+Lemma finite_card_disjoint_union : forall (n : nat) (X Y : Ensemble U), cardinal X n -> Finite Y -> Disjoint U X Y -> Finite (Union U X Y).
 Proof.
   intro n.
   induction n.
@@ -286,88 +286,268 @@ Proof.
   unfold Setminus in H2.
   intuition.
 Qed.
-(*
-Lemma finite_subset : forall X Y : Ensemble U, Included U X Y -> Finite Y -> Finite X.
+
+Lemma finite_subset : forall X : Ensemble U, Finite X -> forall Y, (Included U Y X -> Finite Y).
 Proof.
+  intro X.
+  intro.
+  induction H.
   intros.
-  induction H0.
-  assert (X = Empty_set U).
+  assert (Y = Empty_set U).
   apply Extensionality_Ensembles.
   unfold Same_set.
   split.
   apply H.
   unfold Included.
   intros.
-  destruct H0.
+  contradiction.
   rewrite H0.
   apply Empty_is_finite.
-  apply IHFinite
-  unfold Included in H.
+  intro.
+  assert (In U Y x \/ ~In U Y x).
+  apply classic.
+  case H1.
+  intros.
+  assert (Included U (Subtract U Y x) A).
   unfold Included.
   intros.
-  specialize H with x0.
-  destruct H.
+  unfold In in H4.
+  unfold Subtract in H4.
+  unfold Setminus in H4.
+  destruct H4.
+  unfold In in H5.
+  unfold Included in H3.
+  specialize H3 with x0.
+  apply H3 in H4.
+  unfold In in H4.
+  unfold Add in H4.
+  destruct H4.
+  apply H4.
+  unfold In in H4.
+  contradiction.
+  specialize IHFinite with (Subtract U Y x).
+  apply IHFinite in H4.
+  assert (Add U (Subtract U Y x) x = Y).
+  apply Extensionality_Ensembles.
+  unfold Same_set.
+  split.
+  unfold Included.
+  intros.
+  unfold In in H5.
+  unfold Add in H5.
+  destruct H5.
+  unfold In in H5.
+  unfold Subtract in H5.
+  unfold Setminus in H5.
+  apply H5.
+  unfold In in H5.
+  destruct H5.
   apply H2.
-  apply H.
-  unfold In in H.
-  destruct H.
-  apply H1.
-*)
-(*  
+  unfold Included.
+  intros.
+  unfold In.
+  unfold Add.
+  assert (x = x0 \/ ~(x = x0)).
+  apply classic.
+  case H6.
+  intros.
+  apply Union_intror.
+  unfold In.
+  rewrite H7.
+  apply In_singleton.
+  intro.
+  apply Union_introl.
+  unfold In.
+  unfold Subtract.
+  unfold Setminus.
+  split.
+  apply H5.
+  unfold In.
+  intro.
+  destruct H8.
+  apply H7.
+  reflexivity.
+  rewrite <- H5.
+  apply Union_is_finite.
+  apply H4.
+  intro.
+  unfold In in H6.
+  unfold Subtract in H6.
+  unfold Setminus in H6.
+  unfold In in H6.
+  destruct H6.
+  destruct H7.
+  apply In_singleton.
+  intros.
+  assert (Included U Y A).
+  unfold Included.
+  intros.
+  unfold Included in H3.
+  specialize H3 with x0.
+  assert (x <> x0).
+  intro.
+  subst.
+  contradiction.
+  apply H3 in H4.
+  unfold In in H4.
+  unfold Add in H4.
+  destruct H4.
+  apply H4.
+  unfold In in H4.
+  destruct H4.
+  contradiction.
+  apply IHFinite.
+  apply H4.
+Qed.  
 Lemma union_decompose : forall X Y : Ensemble U, Union U X Y = Union U (Union U (Setminus U X Y) (Intersection U X Y)) (Setminus U Y X).
 Proof.
   intros.
+  assert (forall x : U, In U X x \/ ~In U X x).
+  intro.
+  apply classic.
+  assert (forall x : U, In U Y x \/ ~In U Y x).
+  intro.
+  apply classic.
   apply Extensionality_Ensembles.
   unfold Same_set.
   split.
-  unfold Included.
-  intros.
-  unfold In.
-  unfold In in H.
-  case H.
-  intros.
-  unfold In in H0.
-  assert (X = Union U (Intersection U X Y) (Setminus U X Y)).
-  apply Extensionality_Ensembles.
-  unfold Same_set.
-  split.
-  unfold Included.
-  intros.
-  unfold In.
-  apply Union_introl
-  apply H1.
   unfold Included.
   intros.
   unfold In in H1.
-  case H1.
-  intuition.
-  intuition.
-  unfold In in H2.
-  unfold Setminus in H2.
-  apply H2.
-  assert (In U (Union U (Intersection U X Y) (Setminus U X Y)) x0).
-  rewrite <- H1.
-  apply H0.
-  unfold In in H2.
-  destruct H2.
+  destruct H1.
+  unfold In.
+  specialize H with x.
+  specialize H0 with x.
+  case H0.
+  intros.
   apply Union_introl.
   unfold In.
-  apply Union_introl
+  apply Union_intror.
+  unfold In.
+  apply Intersection_intro.
+  apply H1.
   apply H2.
-  
-  subst.
-  rewrite <- H0 in H1.
+  intro.
+  apply Union_introl.
+  unfold In.
+  apply Union_introl.
+  unfold In.
+  unfold Setminus.
   intuition.
-
-  apply 
-  case H0.
-*)
-  
-(*
+  unfold In.
+  specialize H with x.
+  case H.
+  intro.
+  apply Union_introl.
+  unfold In.
+  apply Union_intror.
+  unfold In.
+  apply Intersection_intro.
+  apply H2.
+  apply H1.
+  intro.
+  apply Union_intror.
+  unfold In.
+  unfold Setminus.
+  intuition.
+  unfold Included.
+  intros.
+  unfold In.
+  unfold In in H1.
+  destruct H1.
+  unfold In in H1.
+  destruct H1.
+  unfold In in H1.
+  unfold Setminus in H1.
+  apply Union_introl.
+  apply H1.
+  unfold In in H1.
+  destruct H1.
+  apply Union_intror.
+  apply H2.
+  unfold In in H1.
+  unfold Setminus in H1.
+  apply Union_intror.
+  apply H1.
+Qed.
+Lemma finite_disjoint_union : forall (X Y : Ensemble U), Finite X -> Finite Y -> Disjoint U X Y -> Finite (Union U X Y).
+Proof.
+  intros.
+  apply finite_cardinal in H.
+  destruct H.
+  apply (finite_card_disjoint_union x X Y).
+  apply H.
+  apply H0.
+  apply H1.
+Qed.  
 Lemma fin_union : forall X Y : Ensemble U, Finite X -> Finite Y -> Finite (Union U X Y).
 Proof.
   intros.
-*)  
-  
-
-  
+  rewrite union_decompose.
+  assert (Finite (Setminus U X Y)).
+  assert (Included U (Setminus U X Y) X).
+  unfold Included.
+  intros.
+  unfold In in H1.
+  unfold Setminus in H1.
+  apply H1.
+  apply (finite_subset X).
+  apply H.
+  apply H1.
+  assert (Finite (Intersection U X Y)).
+  assert (Included U (Intersection U X Y) X).
+  unfold Included.
+  intros.
+  unfold In in H2.
+  destruct H2.
+  apply H2.
+  apply (finite_subset X).
+  apply H.
+  apply H2.
+  assert (Finite (Setminus U Y X)).
+  assert (Included U (Setminus U Y X) Y).
+  unfold Included.
+  intros.
+  unfold In in H3.
+  unfold Setminus in H3.
+  apply H3.
+  apply (finite_subset Y).
+  apply H0.
+  apply H3.
+  assert (Disjoint U (Setminus U X Y) (Intersection U X Y)).
+  apply Disjoint_intro.
+  intros.
+  intro.
+  unfold In in H4.
+  destruct H4.
+  unfold In in H4.
+  unfold Setminus in H4.
+  unfold In in H5.
+  destruct H5.
+  destruct H4.
+  contradiction.
+  assert (Disjoint U (Union U (Setminus U X Y) (Intersection U X Y)) (Setminus U Y X)).
+  apply Disjoint_intro.
+  intros.
+  intro.
+  unfold In in H5.
+  destruct H5.
+  unfold In in H6.
+  unfold Setminus in H6.
+  destruct H6.
+  unfold In in H5.
+  destruct H5.
+  unfold In in H5.
+  unfold Setminus in H5.
+  intuition.
+  unfold In in H5.
+  destruct H5.
+  intuition.
+  apply finite_disjoint_union.
+  apply finite_disjoint_union.
+  apply H1.
+  apply H2.
+  apply H4.
+  apply H3.
+  apply H5.
+Qed.  
